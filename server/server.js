69e9de72a6c210520c877858975a360ec2be70ee
MathomJohnson <156151500+MathomJohnson@users.noreply.github.com>
2024-08-04 22:29:43 -0500

    setup client and server

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors")
+const app = express();
+const corsOptions = {
+    options: ["http://localhost:5173/"],
+};
+
+app.use(cors(corsOptions));
+
+app.get("/api", (req, res) => {
+    res.json({"animals": ["grue", "lois", "scout"]})
+});
+
+app.listen(8080, () => {
+    console.log("Server listening on port 8080")
+});
