9221f849105e77a297dbd478b587799e1c9144d5
MathomJohnson <156151500+MathomJohnson@users.noreply.github.com>
2024-08-05 23:12:59 -0500

    created about me section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
 //import { useEffect, useState } from 'react'
 import './App.css';
 import Navbar from './components/Navbar';
-import { Route, Routes } from "react-router-dom"
 import Skills from './components/pages/Skills'
 import About from './components/pages/About'
 import Projects from './components/pages/Projects'
 import Contact from './components/pages/Contact'
+import Intro from './components/pages/Intro'
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
         <Route path="/projects" element={<Projects />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
       </Routes> */}
+      <Intro />
       <About />
       <Skills />
       <Projects />
--- client/src/components/pages/About.tsx
+++ client/src/components/pages/About.tsx
@@ -3,25 +3,43 @@ import './About.css'
 function About() {
     return (
         <div id="about">
-            <div className="left-content">
-                <img className="mathom-headshot" src="/images/mathom-head.png" alt="Mathom Headshot"></img>
-            </div>
-            <div className="about">
-                <p className="ubuntu-light-italic">My name is:</p>
-                <h1 className="ubuntu-medium">Mathom Johnson</h1>
-                <p className="ubuntu-light-italic">Sophomore @ UW-Madison studying Computer Science</p>
-                <div className="social-media">
-                    <a href="https://www.linkedin.com/in/mathomjohnson">
-                        <img className="logo" src="/images/linkedin-logo.png" alt="LinkedIn Logo"></img>
-                    </a>
-                    <a href="https://github.com/MathomJohnson">
-                        <img className="logo" src="/images/github-logo.png" alt="GitHub Logo"></img>
-                    </a>
-                    <a href="https://www.youtube.com/@MathomJohnson">
-                        <img className="logo" src="/images/youtube-logo.png" alt="YouTube Logo"></img>
-                    </a>
+            <h2 className="ubuntu-medium">Get To Know Me</h2>
+            <div id="about-container">
+                <div className="edu-and-interests">
+                    <div className="education-container my-border">
+                        <div className="education">
+                            <img className="mortarboard-logo" src="/images/mortarboard.png"></img>
+                            <h3 className="ubuntu-medium">Education</h3>
+                        </div>
+                        <div className="education-desc">
+                            <p className="ubuntu-light-italic">
+                                Pursuing a B.S. in Computer Science @ UW-Madison
+                            </p>
+                        </div>
+                    </div>
+                    <div className="interests-container my-border">
+                        <div className="interests">
+                            <img className="magnifying-logo" src="/images/magnifying-glass.png"></img>
+                            <h3 className="ubuntu-medium">Interests</h3>
+                        </div>
+                        <div className="interests-desc">
+                            <p className="ubuntu-light-italic">
+                                Full Stack Development, Machine Learning, Internet Of Things, Applied Math
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="grand-canyon">
+                    <img className="gc-pic" src="/images/grand-canyon.png" alt="Grand Canyon"></img>
                 </div>
             </div>
+            <p className="ubuntu-light-italic about-me">
+                My name is Mathom Johnson and I'm a sophomore studying Computer Science at the University of Wisconsin-Madison.
+                Currently, I am working as a mentor and web developer for a student startup here in Madison called "International Badger Bonds".
+                Our goal is to connect incoming international students with student mentors who can provide assistance while they settle into a new environment.
+                Over the summer I got to meet with countless students from all over the world. Outside of school I am studying Machine Learning through 
+                Coursera and YouTube. I love to apply math to real-world problems that Machine Learning aims to solve.
+            </p>
         </div>
     )
 }
