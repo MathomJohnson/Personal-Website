6e9ea46e4aa8741deac616340be010b1b7cc7065
MathomJohnson <156151500+MathomJohnson@users.noreply.github.com>
2024-08-07 15:07:02 -0500

    adding skills

--- client/src/components/pages/Skills.tsx
+++ client/src/components/pages/Skills.tsx
@@ -1,13 +1,32 @@
+import "./Skills.css"
+
 function Skills() {
+
     return (
         <div id="skills">
             <h1>Skills</h1>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
+            <div className="languages my-border">
+                <div className="python-wrapper">
+                    <img src="/images/python-logo.png" className="python-logo"></img>
+                    <p className="py-text"><b>Python</b></p>
+                </div>
+                <div className="python-wrapper">
+                    <img src="/images/typescript.png" className="python-logo"></img>
+                    <p className="py-text"><b>TypeScript</b></p>
+                </div>
+                <div className="python-wrapper">
+                    <img src="/images/java.png" className="python-logo"></img>
+                    <p className="py-text"><b>Java</b></p>
+                </div>
+                <div className="python-wrapper">
+                    <img src="/images/html.png" className="python-logo smaller-logo"></img>
+                    <p className="py-text"><b>HTML</b></p>
+                </div>
+                <div className="python-wrapper">
+                    <img src="/images/image.png" className="python-logo smaller-logo"></img>
+                    <p className="py-text"><b>JavaScript</b></p>
+                </div>
+            </div>
         </div>
     )
 }
