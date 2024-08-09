580c911e6a9177b00d0504d3c1282198fa8c09a3
MathomJohnson <156151500+MathomJohnson@users.noreply.github.com>
2024-08-09 00:56:28 -0500

    working on projects

--- client/src/components/pages/Projects.tsx
+++ client/src/components/pages/Projects.tsx
@@ -1,17 +1,40 @@
+import './Projects.css'
+
 function Projects() {
     return (
-        <div id="projects">
-            <h1>Projects</h1>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
+        <div id="projects" data-bs-theme="dark">
+            <div id="carouselExampleIndicators" className="carousel slide c">
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div className="carousel-inner c-inner  active">
+                    <div className="carousel-item c-item">
+                        <div className="my-border d-block c-div">
+                            <img src="/images/IBB.png" className="c-img" alt="..."></img>
+                        </div>
+                    </div>
+                    <div className="carousel-item c-item">
+                        <div className="my-border d-block c-div">
+                            <img src="/images/IBB.png" className="c-img" alt="..."></img>
+                        </div>
+                    </div>
+                    <div className="carousel-item c-item">
+                        <div className="my-border d-block c-div">
+                            <img src="/images/IBB.png" className="c-img" alt="..."></img>
+                        </div>
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
         </div>
     )
 }
