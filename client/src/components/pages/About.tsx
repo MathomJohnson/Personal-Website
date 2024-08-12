b9df52c2a79807d68f4d5d199618641510a96f36
MathomJohnson <156151500+MathomJohnson@users.noreply.github.com>
2024-08-11 20:17:48 -0500

    fixed fetch

--- client/src/components/pages/About.tsx
+++ client/src/components/pages/About.tsx
@@ -15,7 +15,7 @@ function About() {
     const [semester, setSemester] = useState("");
 
     useEffect(() => {
-        fetch("personal-website-server-kappa.vercel.app/course-data")
+        fetch("https://personal-website-server-kappa.vercel.app/course-data")
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok ' + response.statusText);
